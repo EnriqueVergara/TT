80c704f650c1a9e1d2a5d57800313fbdfaf448af
bruno <bruno@bruno-Studio-1537>
2015-06-29 01:35:15 -0500

    Se agregaron conceptos y se corrigió un error de ortografía en la vista.

--- TT/src/com/TT/view/FrameAcercaDe.java
+++ TT/src/com/TT/view/FrameAcercaDe.java
@@ -54,7 +54,7 @@ public class FrameAcercaDe extends javax.swing.JFrame {
 
         jLabel2.setText("Escuela Superior de Cómputo");
 
-        jLabel3.setText("Módulo de análisis semántico sobre el análizador sintáctico Freeling");
+        jLabel3.setText("Módulo de análisis semántico sobre el analizador sintáctico Freeling");
 
         jLabel4.setText("Desarrollado como parte del Trabajo Terminal 2014-A024");
 
@@ -115,41 +115,6 @@ public class FrameAcercaDe extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrameAcercaDe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrameAcercaDe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrameAcercaDe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrameAcercaDe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FrameAcercaDe().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
